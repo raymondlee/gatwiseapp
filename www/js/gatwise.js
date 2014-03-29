38de82471a45fe5f19b684b7738adb8e19220c5b
Raymond Lee <raymondlee126@gmail.com>
2014-03-29 16:17:55 +0800

    Add contacts access

--- www/js/gatwise.js
+++ www/js/gatwise.js
@@ -4,15 +4,30 @@ var gatwise = {
             console.log('hello');
     },
 
-    // Show a custom alertDismissed
-    //
-    
-    showAlert: function(){
+   showAlert: function(){
         navigator.notification.alert(
             'You are the winner!',  // message
             this.alertDismissed,         // callback
             'Game Over',            // title
             'Done'                  // buttonName
         );
+    },
+    
+    getContacts: function() {
+        // find all contacts with 'Bob' in any name field
+        var options = new ContactFindOptions();
+        //options.filter = "Bob";
+        var fields = ["displayName"];
+        navigator.contacts.find(fields, this.getContactsSuccess, this.onError, options);
+    },
+    
+    getContactsSuccess: function(aContacts) {
+        for (var i = 0; i < aContacts.length; i++) {
+          console.log("Display Name = " + aContacts[i].displayName);
+        }
+    },
+    
+    getContactsError: function(aError) {
+       alert('onError!');
     }
 };
