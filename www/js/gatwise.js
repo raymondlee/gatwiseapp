c6d8032f8a0396b4e40afd7f812685257ce82ec6
Raymond Lee <raymondlee126@gmail.com>
2014-03-30 04:03:15 +0800

    Update get contacts code and display contacts on index page

--- www/js/gatwise.js
+++ www/js/gatwise.js
@@ -1,33 +1,54 @@
 var gatwise = {
     alertDismissed: function() {
-            // do something            
-            console.log('hello');
+        console.error('hello');
     },
 
-   showAlert: function(){
+   showAlert: function() {
         navigator.notification.alert(
             'You are the winner!',  // message
-            this.alertDismissed,         // callback
+            this.alertDismissed,    // callback
             'Game Over',            // title
             'Done'                  // buttonName
         );
     },
     
     getContacts: function() {
-        // find all contacts with 'Bob' in any name field
         var options = new ContactFindOptions();
-        //options.filter = "Bob";
-        var fields = ["displayName"];
+        options.filter = '';
+        options.multiple = true;
+        var fields = ['*'];  //"*" will return all contact fields
         navigator.contacts.find(fields, this.getContactsSuccess, this.onError, options);
     },
     
     getContactsSuccess: function(aContacts) {
-        for (var i = 0; i < aContacts.length; i++) {
-          console.log("Display Name = " + aContacts[i].displayName);
-        }
+        var li = '';
+
+        $.each(aContacts, function(aContactKey, aContact) {
+            var name, phone;
+            if (aContact.name) {
+                $.each(aContact.name, function(aNameKey, aName) {
+                    if (aNameKey == 'formatted') {
+                        name = aName;
+                    }
+                });
+            }
+            if (aContact.phoneNumbers) {
+                $.each(aContact.phoneNumbers, function(aPhoneNumberKey, aPhoneNumber) {
+                    phone = aPhoneNumber.value;
+                });
+            }
+            if (name) {
+                li += '<li style="text-decoration:none;">' + name;
+                if (phone) {
+                    li += ' ' + phone;
+                }
+                li += '</li>';
+            } 
+        });
+        $("#contacts").html(li);
     },
     
     getContactsError: function(aError) {
-       alert('onError!');
+        alert('onError!');
     }
 };
