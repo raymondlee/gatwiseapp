cb1514129a0333babe7f3a11e726b99c51cae9f2
Raymond Lee <raymondlee126@gmail.com>
2014-04-09 20:47:22 +0800

    Add google map to settings view

--- ionic/www/js/controllers.js
+++ ionic/www/js/controllers.js
@@ -36,6 +36,29 @@ angular.module('starter.controllers', [])
     $location.url('/tab/chats');
   }
 
+  // Set map location
+  var mapOptions = {
+    center: new google.maps.LatLng(43.07493,-89.381388),
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  // Stop the side bar from dragging when mousedown/tapdown on the map
+  google.maps.event.addDomListener(document.getElementById('map-canvas'), 'mousedown', function(e) {
+    e.preventDefault();
+    return false;
+  });
+  $scope.map = map;
+
+  navigator.geolocation.getCurrentPosition(function(aPos) {
+    $scope.map.setCenter(
+      new google.maps.LatLng(aPos.coords.latitude, aPos.coords.longitude));
+  }, function(error) {
+    alert('Unable to get location: ' + error.message);
+  });
+
+  // modal view
   $ionicModal.fromTemplateUrl('templates/modal.html', {
     scope: $scope,
     animation: 'slide-in-up'
@@ -47,6 +70,7 @@ angular.module('starter.controllers', [])
       console.log(aError);
     });
   });
+
   $scope.openModal = function() {
     $scope.modal.show();
   };
